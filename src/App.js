e34d99aeddf8316586e31066db4a5e9c451020b4
Aishwarya <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2026-06-03 10:47:55 +0530

    completed todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [task, setTask] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  const addTask = () => {
+
+    if(task.trim() === "") return;
+
+    const newTask = {
+      text: task,
+      completed: false
+    };
+
+    setTasks([...tasks, newTask]);
+
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+
+    const updatedTasks =
+      tasks.filter((_, i) => i !== index);
+
+    setTasks(updatedTasks);
+  };
+
+  const toggleComplete = (index) => {
+
+    const updatedTasks = [...tasks];
+
+    updatedTasks[index].completed =
+      !updatedTasks[index].completed;
+
+    setTasks(updatedTasks);
+  };
+  const filteredTasks = tasks.filter((t) => {
+
+  if(filter === "completed"){
+    return t.completed;
+  }
+
+  if(filter === "pending"){
+    return !t.completed;
+  }
+
+  return true;
+});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <div className="container">
+
+      <div className="title-section">
+
+        <h1>📝 My To-Do List</h1>
+
+        <p>Stay organized. Get things done.</p>
+
+        <div className="line"></div>
+
+      </div>
+
+      <div className="input-section">
+
+        <input
+          type="text"
+          placeholder="Enter a new task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+
+        <button
+          className="add-btn"
+          onClick={addTask}
         >
-          Learn React
-        </a>
-      </header>
+          Add Task +
+        </button>
+
+      </div>
+
+      <div className="filter-section">
+
+  <button
+    className={
+      filter === "all"
+      ? "filter-btn active"
+      : "filter-btn"
+    }
+
+    onClick={() => setFilter("all")}
+  >
+    All ({tasks.length})
+  </button>
+
+  <button
+    className={
+      filter === "completed"
+      ? "filter-btn active"
+      : "filter-btn"
+    }
+
+    onClick={() => setFilter("completed")}
+  >
+    Completed (
+    {
+      tasks.filter(t => t.completed).length
+    }
+    )
+  </button>
+
+  <button
+    className={
+      filter === "pending"
+      ? "filter-btn active"
+      : "filter-btn"
+    }
+
+    onClick={() => setFilter("pending")}
+  >
+    Pending (
+    {
+      tasks.filter(t => !t.completed).length
+    }
+    )
+  </button>
+
+</div>
+
+      <ul>
+
+        {filteredTasks.map((t, index) => (
+
+          <li
+            key={index}
+            className={
+              t.completed ? "completed-task" : ""
+            }
+          >
+
+            <div className="task-left">
+
+              <input
+                type="checkbox"
+                checked={t.completed}
+                onChange={() =>
+                  toggleComplete(index)
+                }
+              />
+
+              <div>
+
+                <span
+                  className={
+                    t.completed
+                    ? "completed"
+                    : ""
+                  }
+                >
+                  {t.text}
+                </span>
+
+                <p>No due date</p>
+
+              </div>
+
+            </div>
+
+            <button
+              className="delete-btn"
+              onClick={() =>
+                deleteTask(index)
+              }
+            >
+              🗑
+            </button>
+
+          </li>
+
+        ))}
+
+      </ul>
+
     </div>
   );
 }
